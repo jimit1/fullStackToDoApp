3521ebe33107adf54b18575270658587440875c1
Jimit Mehta <jimitmehta@MacBook-Pro.local>
2020-06-24 19:52:40 -0700

    more prep for deploy

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
-const e = require("express");
 
-const connection = mysql.createConnection({
-  // we are using a method that is looking for an object, with a specific key
-  host: "localhost", // optimal for futur deployement
-  port: 3306, // never change this value
-  user: "root", // never change it
-  password: "Hello135", // it is better to use a .env file to not expose our password
-  database: "todo_db", // the db we created in workbench
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    // we are using a method that is looking for an object, with a specific key
+    host: "localhost", // optimal for futur deployement
+    port: 3306, // never change this value
+    user: "root", // never change it
+    password: "Hello135", // it is better to use a .env file to not expose our password
+    database: "todo_db", // the db we created in workbench
+  });
+}
 
 module.exports = connection;
